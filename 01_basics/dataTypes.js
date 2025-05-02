f10657a10ebfaf3ef32da69b973e42d9b2a930cc
Uzmaalia <118560215+Uzmaalia@users.noreply.github.com>
2025-05-02 11:21:26 +0000

    dataTypes

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,32 @@
+"use strict"; // treate all js code as new version
+
+//alert(3+3)  //can not use as we are using nodejs not browser
+
+console.log(3 
+    +
+    3) //code readibility should be high
+
+
+console.log("Uzma");
+    
+let name = "Uzma"
+let age = 18
+let isLoggedIn = false
+let state = null;
+
+// Number --> 2 to power 53
+// bigint
+// string --> ""
+// boolean --> true/false
+// null --> standalnoe value (representaion of empty value)
+//undefined --> no data ype is defined
+// symbol --> unique
+
+
+//object
+
+console.log(typeof "uzma")
+
+console.log(typeof null) //object
+
+console.log(typeof undefined) //undefined
