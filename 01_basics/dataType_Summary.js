936a37dd7417f02e29235d90fd411e96efe5a690
Uzmaalia <118560215+Uzmaalia@users.noreply.github.com>
2025-05-03 15:48:01 +0000

    DT Summary

---
+++ 01_basics/dataType_Summary.js
@@ -0,0 +1,33 @@
+const score = 100;
+const scoreValue = 100.3;
+
+const isLoggedIn = false;
+const outsideTemp = null;
+
+let userEmail; //undefined data type
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id == anotherId);
+
+const bigNum = 453063507515347499n
+
+
+//Reference (Non Primitive)
+
+const fruits = ["apple" , "orange" , "kiwi"]
+
+//Object
+let myObj = {
+    name : "Anni",
+    age : 22 ,
+}
+
+//Function
+const myFuncn = function(){
+    console.log("Hello world");
+    
+}
+
+console.log(typeof bigNum);
