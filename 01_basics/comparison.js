0066bbd4b93906077297cad47301894d2fd1894d
Uzmaalia <118560215+Uzmaalia@users.noreply.github.com>
2025-05-03 14:29:36 +0000

    comparison

---
+++ 01_basics/comparison.js
@@ -0,0 +1,24 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+/**AVOID THIS TYPE OF COMPARISON */
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+
+// === --> strict check --> no conversion occurs here
+console.log("2" == 2);
+
