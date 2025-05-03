4a193bf17288aaa1f2507bbc88d767a8c7487807
Uzmaalia <118560215+Uzmaalia@users.noreply.github.com>
2025-05-03 14:12:56 +0000

    operations

---
+++ 01_basics/operations.js
@@ -0,0 +1,36 @@
+let val = 3
+let negVal = -val
+// console.log(negVal);
+
+let str1 = "Hello"
+let str2 = " Uzma"
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log((3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+// let num1, num2, num3
+// num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+// gameCounter++;
+// console.log(gameCounter);
+
+
+
+
+
+
+
+
+
