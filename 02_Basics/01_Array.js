758543a82fba23ee593a9562d1d826419a0509a0
Uzmaalia <118560215+Uzmaalia@users.noreply.github.com>
2025-05-04 14:16:35 +0000

    Array

---
+++ 02_Basics/01_Array.js
@@ -0,0 +1,42 @@
+// **************************************ARRAY*************************************
+
+// const arr = [0,1,2,3,4,5,true,"array"]
+
+const arr = [0,1,2,3,4,5]
+const fruit = ["apple","mango"]
+
+// console.log(arr[1]);
+
+
+//ARRAY METHODS
+
+// arr.push(6)
+// arr.push(7)
+// arr.pop()
+// console.log(arr);
+
+// arr.unshift(8)
+// arr.shift()
+
+// console.log(arr.includes(9));
+// console.log(arr.indexOf(9));
+
+// const newarr = arr.join()
+
+// console.log(arr);
+// console.log(newarr);
+
+
+//SLICE and SPLICE
+
+console.log("A" , arr);
+
+const arr1  = arr.slice(1,3)
+
+console.log(arr1);
+console.log("B",arr);
+
+const arr2  = arr.splice(1,3)
+console.log("c" , arr);
+console.log(arr2);
+
