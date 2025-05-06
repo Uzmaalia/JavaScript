a90e166481df81c676cc260f3172e1047e19cf1a
Uzmaalia <118560215+Uzmaalia@users.noreply.github.com>
2025-05-06 15:35:55 +0000

    Functions

--- 03_Basics/01_Functions.js
+++ 03_Basics/01_Functions.js
@@ -35,4 +35,39 @@ function logInUserMessage(usernam = "sam"){
 }
 
 //  console.log(logInUserMessage("uzma"));
- console.log(logInUserMessage()); //--->Undefined
+//console.log(logInUserMessage()); //--->Undefined
+
+
+
+function calculateCartPrice(val1,val2, ...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(200,400,500,2000));
+
+const user ={
+    usernam : "jojo",
+    price: 199
+
+}
+
+function handleObject(anyObject){
+    console.log(`username is ${anyObject.usernam} and price is ${anyObject.price}`);
+    
+}
+
+// handleObject(user)
+handleObject({
+    user:'sam',
+    price: 399
+})
+
+const arr = [200, 400, 600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+//  console.log( returnSecondValue(arr));
+ console.log( returnSecondValue([200, 400, 500, 1000]));
+
+ 
