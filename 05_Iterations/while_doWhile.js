e59401f5dd6f48701c7b1d38669f4c8bfb88645f
Uzmaalia <118560215+Uzmaalia@users.noreply.github.com>
2025-05-08 19:34:09 +0000

    while and do while

---
+++ 05_Iterations/while_doWhile.js
@@ -0,0 +1,24 @@
+// let index = 0
+// while (index <= 10) {
+//     console.log(`Value of index ${index}`);
+//     index = index + 2
+// }
+
+// let myarr = ['aple' , 'kiwi' , 'orange']
+
+// let arr = 0
+// while (arr < myarr.length) {
+//     console.log(`Value is ${myarr[arr]}`);
+//     arr++
+// }
+
+
+let score = 1
+
+do{
+    console.log(`score is ${score}`);
+    score++
+}
+while (score <= 10) {
+    
+}
