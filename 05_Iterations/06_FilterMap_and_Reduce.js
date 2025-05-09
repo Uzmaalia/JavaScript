2f2d8ca6e9c73cd88ff09d91a61259eb790794c4
Uzmaalia <118560215+Uzmaalia@users.noreply.github.com>
2025-05-09 15:09:35 +0000

    File Map and Reduce

---
+++ 05_Iterations/06_FilterMap_and_Reduce.js
@@ -0,0 +1,30 @@
+const coding = [ 'js', 'rubby', 'java', 'python' , 'cpp']
+
+// const values = coding.forEach( (item) => {
+//     console.log(item);
+//     return item
+// })
+
+// console.log(values);  // will return undefined
+
+
+const mynums = [1,2,3,4,5,6,7,8,9,10]
+
+//const newnum = mynums.filter( (num) => num > 4)
+// console.log(newnum);
+
+// const newnum = mynums.filter( (num) => {
+//     return num>4
+// })
+// console.log(newnum);
+
+const newnum = []
+
+mynums.forEach( (num) => {
+    if (num>4) {
+        newnum.push(num)
+    }
+})
+console.log(newnum);
+
+
