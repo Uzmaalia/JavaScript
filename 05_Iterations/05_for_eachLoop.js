84c3a6888a663cf204af4c931cfc458d4cf5d548
Uzmaalia <118560215+Uzmaalia@users.noreply.github.com>
2025-05-09 13:49:46 +0000

    for each loop

---
+++ 05_Iterations/05_for_eachLoop.js
@@ -0,0 +1,45 @@
+const  coding = ['js','python','java', 'cpp']
+
+// coding.forEach( function(val) {   //call back function
+//     console.log(val);
+    
+// }) 
+
+// coding.forEach((item) => {   //call back with arrow funcn
+// console.log(item);
+
+// })
+
+
+// function print(item){
+//     console.log(item);
+    
+// }
+
+// coding.forEach(print)
+
+// coding.forEach( (item, index, arr)=> {
+//     console.log(item, index, arr);
+    
+// } )
+
+
+const mycode = [
+    {
+        language: 'javascript',
+        extension: 'js'
+    },
+    {
+        language: 'python',
+        extension: 'py'
+    },
+    {
+        language: 'html',
+        extension: 'html'
+    },
+]
+
+mycode.forEach( (item)=> {
+    console.log(item.language);
+    
+})
