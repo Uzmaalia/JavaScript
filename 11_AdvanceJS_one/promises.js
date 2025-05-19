559397ff80163f4ada14b07f3d86bd690254c28a
Uzmaalia <118560215+Uzmaalia@users.noreply.github.com>
2025-05-19 16:33:40 +0000

    promises

---
+++ 11_AdvanceJS_one/promises.js
@@ -0,0 +1,95 @@
+const promiseOne = new Promise(function(resolve,reject){
+    //Do an Async task
+    //DB calls, cryptography, network
+    setTimeout(function(){
+        console.log('Async task is complete');
+        resolve()
+        
+    },1000)
+})
+
+promiseOne.then(function(){
+    console.log('Promise consumed');
+    
+})
+
+
+// *******************OR************************
+
+new Promise(function(resolve,reject){
+    setTimeout(function(){
+        console.log('Async task 2');
+        resolve()
+        
+    },1000)
+}).then(function(){
+    console.log('Async task 2 resolved');
+    
+})
+
+
+const promiseThree = new Promise(function(resolve,reject){
+    setTimeout(function(){
+        resolve({username: 'cat', email: 'cat@example.com'}) 
+    },1000)
+})
+
+promiseThree.then(function(user){
+    console.log('Promise consumed');
+    console.log(user);
+    
+    
+})
+
+//BEST APPROACH
+
+const promiseFour = new Promise(function(resolve,reject){
+    setTimeout(function(){
+        let error = false
+        if(!error){
+        resolve({username: 'cat', password: '123'}) 
+        }
+        else{
+            reject('ERROR : Something went wrong')
+        }
+    },1000)
+})
+
+promiseFour
+.then((user) => {
+    console.log(user);
+    return user.username
+})
+.then((username) => {
+    console.log(username);
+})
+.catch(function(error){
+    console.log(error);
+})
+.finally(() => console.log('The promise is either resolved or rejected')
+)
+
+
+const promiseFive = new Promise(function(resolve,reject){
+    setTimeout(function(){
+        let error = false
+        if(!error){
+        resolve({username: 'javascript', password: '123'}) 
+        }
+        else{
+            reject('ERROR : JS went wrong')
+        }
+    },1000)
+})
+
+async function consumepromiseFive() {
+   try {
+     const response = await promiseFive
+     console.log(response);
+   } catch (error) {
+    console.log(error);
+    
+   }
+}
+
+consumepromiseFive()
