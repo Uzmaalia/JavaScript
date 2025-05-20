2b2484e7a8473c74af73e5f42c02cc474cf259ca
Uzmaalia <118560215+Uzmaalia@users.noreply.github.com>
2025-05-20 18:44:28 +0530

    inheritance

---
+++ 12_classes_and_oops/staticprop.js
@@ -0,0 +1,29 @@
+class User {
+    constructor(username){
+        this.username=username
+    }
+    logMe(){
+        console.log(`USERNAME is ${this.username}`);
+    }
+
+    static createId(){
+        return '123'
+    }
+}
+
+const hitesh = new User('hitesh')
+// console.log(hitesh.createId());
+
+class Teacher extends User{
+    constructor(username, email){
+        super(username)
+        this.email
+    }
+}
+
+const iphone = new Teacher('iphone', 'i@phone.com')
+// iphone.logMe
+// console.log(iphone);
+console.log(iphone.createId());
+
+
