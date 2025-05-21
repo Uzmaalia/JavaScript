00d844a0bafd0e812339a185786f5f0921b46bc1
Uzmaalia <118560215+Uzmaalia@users.noreply.github.com>
2025-05-21 12:28:16 +0530

    getter ana setter

---
+++ 12_classes_and_oops/getter_setter.js
@@ -0,0 +1,31 @@
+class User {
+    constructor (email,password) {
+        this.email
+        this.password
+    }
+
+    get email(){
+        return this._email.toUpperCase()
+
+    }
+    set email(value){
+        this._email = value.toUpperCase()
+        // this._password = value
+
+    }
+
+    get password(){
+        // return this._password.toUpperCase()
+        return `${this._password}hitesh`
+
+    }
+    set password(value){
+        // this._password = value.toUpperCase()
+        this._password = value
+
+    }
+}
+
+const hitesh = new User('hitesh@h.ai', '123')
+console.log(hitesh.password);
+
