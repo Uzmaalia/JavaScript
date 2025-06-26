2a7e9c40bf80a806026c8942adb5288ffe093ef6
UzmaAalia <118560215+Uzmaalia@users.noreply.github.com>
2025-06-26 15:56:16 +0000

    filter exercise

--- 05_Iterations/06_FilterMap_and_Reduce.js
+++ 05_Iterations/06_FilterMap_and_Reduce.js
@@ -6,11 +6,13 @@ const coding = [ 'js', 'rubby', 'java', 'python' , 'cpp']
 // })
 
 // console.log(values);  // will return undefined
+//for each loop return no value(undefined)
+
 
 
 const mynums = [1,2,3,4,5,6,7,8,9,10]
 
-//const newnum = mynums.filter( (num) => num > 4)
+//const newnum = mynums.filter( (num) => num > 4)  //returns value unlike for each
 // console.log(newnum);
 
 // const newnum = mynums.filter( (num) => {
@@ -27,4 +29,21 @@ mynums.forEach( (num) => {
 })
 console.log(newnum);
 
+const books = [
+    { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+    { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+    { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+    { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+    { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+    { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+    { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+    { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+    { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+  ];
 
+  let userBooks = books.filter( (bk) => bk.genre === 'History')
+
+  userBooks = books.filter( (bk) => { 
+    return bk.publish >= 1995 && bk.genre === "History"
+})
+  console.log(userBooks);
