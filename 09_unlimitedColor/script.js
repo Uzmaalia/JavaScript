34cf300741f7ca83459f8a25b326cf1c89a8852c
UzmaAalia <118560215+Uzmaalia@users.noreply.github.com>
2025-07-16 08:23:46 +0000

    update

--- 09_unlimitedColor/script.js
+++ 09_unlimitedColor/script.js
@@ -1,16 +1,18 @@
 const randomColor = function(){
     const hex = "0123456789ABCDEF"
-    let color = '#'
+    let color = '#' //'#' is usually the beginning of a hex color code in CSS (e.g., #ff0000 for red).
+                    // But in this line alone, it doesn’t yet represent a full color.
+
     for(let i=0; i<6; i++){
         color += hex[Math.floor(Math.random() * 16)]
     }
     return color 
 }
 
-let intervalId
+let intervalId //global
 
 const startChangingColor = function(){
-    if (!intervalId) {
+    if (!intervalId) {  //safety check for intervalId = null
    intervalId = setInterval(changeBgColor, 1000) 
     }
 
@@ -21,7 +23,7 @@ const startChangingColor = function(){
 
 const stopChangingColor = function(){
     clearInterval(intervalId)
-    intervalId = null
+    intervalId = null //km hony k bd value derefrence kr dia
 }
 
 
